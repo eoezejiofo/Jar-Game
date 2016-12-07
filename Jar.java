e434ad820e24b05f7253562d5cf25c3cb93d5b44
eoezejiofo <eoezejiofo@gmail.com>
2016-12-07 10:28:16 -0600

    Add files via upload

---
+++ Jar.java
@@ -0,0 +1,20 @@
+
+public class Jar {
+  
+    private String itemName;
+    private int maxNum;
+
+public Jar(String itemName, int maxNum) {
+      this.itemName = itemName;
+      this.maxNum = maxNum;
+  }
+  
+
+  public String getItemName() {
+      return itemName;
+  }
+  
+  public int getMaxNum() {
+      return maxNum;
+  }
+}
